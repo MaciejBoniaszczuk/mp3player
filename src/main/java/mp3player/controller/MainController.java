033563898ba85f84f22209afef3cd5be146aef39
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-02-10 21:39:31 +0100

    feat: dividing the code into fxml modules

--- src/main/java/mp3player/controller/MainController.java
+++ src/main/java/mp3player/controller/MainController.java
@@ -6,41 +6,16 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
-    @FXML
-    private MenuItem fileMenultem;
 
-    @FXML
-    private MenuItem dirMenultem;
-
-    @FXML
-    private MenuItem closeMenultem;
-
-    @FXML
-    private MenuItem aboutMenultem;
-
-    @FXML
-    private TableView<?> contentTable;
-
-    @FXML
-    private Button previousButton;
-
-    @FXML
-    private ToggleButton playButton;
-
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private Slider volumeSlider;
-
-    @FXML
-    private Slider progressSlider;
 
     public void initialize(){
-        System.out.println("MainController created");
+        System.out.println("Main controller created");
     }
+
+
 
 }
