5edfad5d4a145f42a566d283fb30f9a9fe8c39ca
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-02-10 21:19:47 +0100

    feat: main controllers added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,5 @@ module mp3player {
     requires javafx.controls;
     requires javafx.fxml;
     exports mp3player.main to javafx.graphics;
+    opens mp3player.controller to javafx.fxml;
 }
---
+++ src/main/java/mp3player/controller/MainController.java
@@ -0,0 +1,46 @@
+package mp3player.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+
+public class MainController {
+
+    @FXML
+    private MenuItem fileMenultem;
+
+    @FXML
+    private MenuItem dirMenultem;
+
+    @FXML
+    private MenuItem closeMenultem;
+
+    @FXML
+    private MenuItem aboutMenultem;
+
+    @FXML
+    private TableView<?> contentTable;
+
+    @FXML
+    private Button previousButton;
+
+    @FXML
+    private ToggleButton playButton;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private Slider progressSlider;
+
+    public void initialize(){
+        System.out.println("MainController created");
+    }
+
+}
