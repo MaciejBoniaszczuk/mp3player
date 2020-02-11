f0a78817d7a62d510dd1edc55c83c344bf144459
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-02-11 09:33:06 +0100

    feat: add css

--- src/main/java/mp3player/controller/MainController.java
+++ src/main/java/mp3player/controller/MainController.java
@@ -10,10 +10,19 @@ import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
+    @FXML
+    private ContentPaneController contentPaneController;
+    @FXML
+    private ControlPaneController controlPaneController;
+    @FXML
+    private MenuPaneController menuPaneController;
 
 
     public void initialize(){
         System.out.println("Main controller created");
+        System.out.println(contentPaneController);
+        System.out.println(controlPaneController);
+        System.out.println(menuPaneController);
     }
 
 
