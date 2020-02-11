8fffd5a80ef5b9c66aa6feae2846f06b2402ad4c
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-02-11 10:19:18 +0100

    feat: media logic added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module mp3player {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires jid3lib;
+    requires javafx.media;
+
     exports mp3player.main to javafx.graphics;
     opens mp3player.controller to javafx.fxml;
     opens mp3player.mp3 to javafx.base;
